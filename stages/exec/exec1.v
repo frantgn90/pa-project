24f0263affb2305d614c57189c17c82c6b18611b
sergi <sapcat7@gmail.com>
2017-01-13 15:02:09 +0100

    Added is_byte signal through exec1

--- stages/exec/exec1.v
+++ stages/exec/exec1.v
@@ -19,6 +19,7 @@ module exec1(
              input wire [`REG_ADDR-1:0]  dst_reg_in, //Destination Register
              input wire                  do_read,//memory read permission
              input wire                  do_write,
+             input wire                  is_read,
              input wire                  memtoreg,//take data from dcache or exec1
              input wire                  is_branch_in,//if it is a branch
 
@@ -30,6 +31,7 @@ module exec1(
              output reg [`ADDR_SIZE-1:0] pc_branch = 32'h0000, //New PC when branch,
              output reg                  do_read_out,
              output reg                  do_write_out,
+             output reg                  is_byte_out,
              output reg                  memtoreg_out,
              output reg                  is_branch_out,
              output reg [`REG_ADDR-1:0]  dst_reg //Destination Register
@@ -57,6 +59,7 @@ module exec1(
          dst_reg <= {`REG_ADDR{1'b0}};
          regwrite_out <= 1'b0;
          do_write_out <= 1'b0;
+         is_byte_out <= 1'b0;
       end else if (we) begin
       is_branch_out <= is_branch_in;
       do_read_out <= do_read;
@@ -69,6 +72,8 @@ module exec1(
 		  alu_result <= aluresult;
       dst_reg <= dst_reg_in;
       regwrite_out <= regwrite_in;
+      is_byte_out <= is_byte_out;
+
 	    end // else: !if(reset)
    end // always @ (posedge clk)
 
